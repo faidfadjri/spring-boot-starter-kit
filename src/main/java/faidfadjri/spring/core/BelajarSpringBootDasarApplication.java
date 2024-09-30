d11b12d40129c1a27e44e36fd278dbef2151063d
faidfadjri <faidfadjri.busdev@gmail.com>
2024-09-30 20:12:20 +0700

    save

---
+++ src/main/java/faidfadjri/spring/core/BelajarSpringBootDasarApplication.java
@@ -0,0 +1,13 @@
+package faidfadjri.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringBootDasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringBootDasarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/faidfadjri/spring/core/BelajarSpringBootDasarApplicationTests.java
@@ -0,0 +1,13 @@
+package faidfadjri.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringBootDasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
